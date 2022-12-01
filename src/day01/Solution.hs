85f8f92df7afa780376ca91d7f5bd38bda071beb
Luke Stead <luke.stead@glean.co>
2022-12-01 13:50:45 +0000

    Remove commented out code

--- src/day01/Solution.hs
+++ src/day01/Solution.hs
@@ -2,15 +2,6 @@ import Utils
 import Data.List.Split
 import Data.List (sort)
 
---solve :: (Show ans) => Int -> (String -> ans) -> (String -> ans) -> IO ()
---solve q p1 p2 = do
---    let inputFile = "src/Q" ++ show q ++ "/input.txt"
---    handle <- openFile inputFile ReadMode
---    contents <- hGetContents handle
---    print ("Part 1: " ++ show (p1 contents))
---    print ("Part 2: " ++ show (p2 contents))
---    hClose handle
-
 parseInput :: String -> [[Int]]
 parseInput x = map (map read) (splitWhen (=="") (lines x))
 
