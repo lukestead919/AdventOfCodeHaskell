359460f4a398f74052ede63b49e13c767fc94231
Luke Stead <luke.stead@glean.co>
2022-12-02 22:35:38 +0000

    Formatting and timing

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,14 +1,20 @@
 module Utils
-    ( solve
-    ) where
+  ( solve,
+  )
+where
 
 import System.IO
+import System.TimeIt
 
 solve :: (Show ans1, Show ans2) => String -> (String -> ans1) -> (String -> ans2) -> IO ()
 solve q p1 p2 = do
-    let inputFile = "src/inputs/Day" ++ q ++ ".txt"
-    handle <- openFile inputFile ReadMode
-    contents <- hGetContents handle
-    putStrLn ("Part 1: " ++ show (p1 contents))
-    putStrLn ("Part 2: " ++ show (p2 contents))
-    hClose handle
+  let inputFile = "src/inputs/Day" ++ q ++ ".txt"
+  handle <- openFile inputFile ReadMode
+  contents <- hGetContents handle
+  timeIt
+    ( do
+        putStrLn ("Part 1: " ++ show (p1 contents))
+        putStrLn ("Part 2: " ++ show (p2 contents))
+    )
+
+  hClose handle
--- src/solutions/Day01.hs
+++ src/solutions/Day01.hs
@@ -1,9 +1,9 @@
-import Utils
-import Data.List.Split
 import Data.List (sort)
+import Data.List.Split
+import Utils
 
 parseInput :: String -> [[Int]]
-parseInput = map (map read) . splitWhen (=="") . lines
+parseInput = map (map read) . splitWhen (== "") . lines
 
 totals :: [[Int]] -> [Int]
 totals = map sum
@@ -12,7 +12,7 @@ part1 :: String -> Int
 part1 = maximum . totals . parseInput
 
 part2 :: String -> Int
-part2 = sum . take 3 . reverse . sort . totals . parseInput 
+part2 = sum . take 3 . reverse . sort . totals . parseInput
 
 main :: IO ()
 main = solve "01" part1 part2
