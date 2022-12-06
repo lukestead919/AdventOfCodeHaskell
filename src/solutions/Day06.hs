a70ca30f453911f958e5bfa0fc8b77c78d736ad8
Luke Stead <luke.stead@glean.co>
2022-12-06 08:51:50 +0000

    Day 6 - reformat

--- src/solutions/Day06.hs
+++ src/solutions/Day06.hs
@@ -1,11 +1,12 @@
-import Data.List (nub, findIndex)
-import Utils
+import Data.List (findIndex, nub)
 import Data.Maybe
+import Utils
 
 findUniqueList :: Int -> String -> Int
-findUniqueList a x = let sublists = [ take a . drop i $ x | i<-[0..(length x)]]
-                         index = fromJust . findIndex (\sublist -> length (nub sublist) == a) $ sublists
-                     in a + index
+findUniqueList a x =
+  let sublists = [take a . drop i $ x | i <- [0 .. (length x)]]
+      index = fromJust . findIndex (\sublist -> length (nub sublist) == a) $ sublists
+   in a + index
 
 part1 :: String -> Int
 part1 = findUniqueList 4
